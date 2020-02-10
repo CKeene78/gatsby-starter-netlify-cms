da413b3bba94f4fc36f93013adcfbeb1cb6125c3
Chris Keene <39811430+CKeene78@users.noreply.github.com>
2020-02-09 21:28:53 -0500

    Trying to block ignore certain sections.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,15 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        <!--div className="content has-text-centered">
+        /*
+        <div className="content has-text-centered">
           <img
             src={logo}
             alt="Kaldi"
             style={{ width: '14em', height: '10em' }}
           />
-        </div-->
+        </div>
+        */
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
