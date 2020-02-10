09978b0dcd2473421c7b9422963604c14e322ea0
Christopher Keene <ckeene@outlook.com>
2020-02-09 22:05:53 -0500

    Updated menus to hid unused pages.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,15 @@ import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
+//import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+//import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        /*
+        {/*
         <div className="content has-text-centered">
           <img
             src={logo}
@@ -19,7 +19,7 @@ const Footer = class extends React.Component {
             style={{ width: '14em', height: '10em' }}
           />
         </div>
-        */
+        */}
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
@@ -36,11 +36,13 @@ const Footer = class extends React.Component {
                         About
                       </Link>
                     </li>
+                    {/*
                     <li>
                       <Link className="navbar-item" to="/products">
                         Products
                       </Link>
                     </li>
+                    */}
                     <li>
                       <Link className="navbar-item" to="/contact/examples">
                         Form Examples
@@ -91,6 +93,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
+                {/*
                 <a title="instagram" href="https://instagram.com">
                   <img
                     src={instagram}
@@ -98,6 +101,8 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
+                */}
+                {/* 
                 <a title="vimeo" href="https://vimeo.com">
                   <img
                     src={vimeo}
@@ -105,6 +110,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
+                */}
               </div>
             </div>
           </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,18 +63,22 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
+              {/*
               <Link className="navbar-item" to="/products">
                 Products
               </Link>
+              */}
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
+              {/*
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link>
+              */}
             </div>
             <div className="navbar-end has-text-centered">
               <a
@@ -83,9 +87,11 @@ const Navbar = class extends React.Component {
                 target="_blank"
                 rel="noopener noreferrer"
               >
+                {/*
                 <span className="icon">
                   <img src={github} alt="Github" />
                 </span>
+                */}
               </a>
             </div>
           </div>
